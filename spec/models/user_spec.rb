416b81e296a233b52cca0de24cc801230b5e9b36
ynghr1226 <74960629+ynghr1226@users.noreply.github.com>
2020-12-17 14:28:27 +0900

    ユーザー管理機能：修正_3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -54,6 +54,11 @@ describe User do
         @user.valid?
         expect(@user.errors.full_messages).to include 'Password is invalid'
       end
+      it 'passwordが全角では登録できない' do
+        @user.password = 'ＡＡＡＡＡ１１'
+        @user.valid?
+        expect(@user.errors.full_messages).to include 'Password is invalid'
+      end
       it 'passwordが存在してもpassword_confirmationが空では登録できない' do
         @user.password_confirmation = ''
         @user.valid?
