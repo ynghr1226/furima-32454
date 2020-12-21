45448c2f418bdedf51eccd26da17d4fe8372ffc6
ynghr1226 <74960629+ynghr1226@users.noreply.github.com>
2020-12-21 21:45:21 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,12 +38,12 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  #     item = Item.find(params[:id])
-  #     if item.destroy
-  #       redirect_to root_path
-  #     end
-  # end
+  def destroy
+      item = Item.find(params[:id])
+      if item.destroy
+        redirect_to root_path
+      end
+  end
 
   private
 
